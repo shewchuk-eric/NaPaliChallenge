d864326e1ad5f4644caccccab939f66677388b41
Eric Shewchuk <eric@thebanyanteam.com>
2023-10-18 21:22:20 -1000

    created some structure

--- header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Na Pali Challenge</title>
-    <!--<script src="javascript/behaviors.js">-->
-    <link rel="stylesheet" href="css/styles.css"/>
---
+++ views/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Eric Shewchuk">
+    <meta name="description" content="The Na Pali Challenge is a world-class outrigger canoe race that spans 37 miles of breathtakingly beautiful coastline.">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <title><?php $title; ?></title>
+    <link rel="stylesheet" href="css/styles.css">
+    <meta property="og:title" content="The World's Most Beautiful Outrigger Race">
+    <meta property="og:type" content="article">
+    <meta property="og:description" content="The Na Pali Challenge spans 37 miles of breathtaking scenery between Hanalei and Kekaha.">
+    <meta property="og:image" content="images/canoe-racing.webp">
+    <meta property="og:url" content="https://shewchuk-eric.github.io/wdd230/">
+    <meta property="og:site_name" content="The Clever Primate">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:image:alt" content="image of a thoughtful looking monkey">
+</head>
+<body>
+<header>
+    <img src="http://localhost:3000/phpmotors/images/logo.png" alt="image of PHP Motors logo">
+    <span id="account"><a href='/phpmotors/accounts/index.php?action=login' title='Returning User Login'>My Account</a></span>
+    <?php include_once 'views/navigation.php'; ?>
+</header>
+    <main>
